a26c6ec0fe622116c2fc42d392b9171a48205b1e
Bruno777202 <b.medeiros@edu.senai.br>
2026-08-17 11:14:38 -0300

    exercicio

---
+++ Aula-06/lista execicio/Exercicio-05/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stack<string> pilhasHistorico = new Stack<string>();
+
+        pilhasHistorico.Push("Digitar");
+        pilhasHistorico.Push("Colar");
+        pilhasHistorico.Push("Negrito");
+        pilhasHistorico.Push("Apagar");
+
+
+        
+
+    }
+}
---
+++ Aula-06/lista execicio/Manipulacaodelist/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    public class Numeros
+    {
+        public int numero {get; set;}
+    }
+    private static void Main(string[] args)
+    {
+        List<Numeros> listanumeros = new List<Numeros>();
+        Numeros numero1 = new Numeros{numero = 10};
+        Numeros numero2 = new Numeros{numero = 25};
+        Numeros numero3 = new Numeros{numero = 8};
+        Numeros numero4 = new Numeros{numero = 42};
+        Numeros numero5 = new Numeros{numero = 15};
+        Numeros numero6 = new Numeros{numero = 30};
+
+
+    }
+}
